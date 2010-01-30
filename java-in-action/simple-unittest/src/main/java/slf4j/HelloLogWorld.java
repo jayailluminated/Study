984cfb1d82bcf1119948039c1bd199dd721aa433
Seo JungJoo <moretajoo@gmail.com>
2010-01-31 02:28:15 +0900

    slf4j binding with log4j

--- java-in-action/simple-unittest/src/main/java/slf4j/HelloLogWorld.java
+++ java-in-action/simple-unittest/src/main/java/slf4j/HelloLogWorld.java
@@ -4,8 +4,31 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class HelloLogWorld {
-  public static void main(String[] args) {
-    Logger logger = LoggerFactory.getLogger(HelloLogWorld.class);
-    logger.info("Hello World");
-  }
+
+	final	Logger logger = LoggerFactory.getLogger(HelloLogWorld.class);
+	Integer t;
+	Integer oldT;
+
+	public void printLogLevel() {
+		logger.debug(HelloLogWorld.class.getName());
+
+		logger.trace("Hello World");
+		logger.debug("Hello World");
+		logger.info("Hello World");
+		logger.warn("Hello World");
+		logger.error("Hello World");
+
+		
+	}
+
+	public void usingSlf4jPlaceholder(Integer temperature) {
+		oldT = t;
+		t = temperature;
+
+		logger.debug("Temperature set to {}. Old temperature was {}.", t, oldT);
+
+		if (temperature.intValue() > 50) {
+			logger.info("Temperature has risen above 50 degrees.");
+		}
+	}
 }
---
+++ java-in-action/simple-unittest/src/test/java/slf4j/HelloLogWorldTest.java
@@ -0,0 +1,37 @@
+package slf4j;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jungjooseo
+ * Date: Jan 31, 2010
+ * Time: 1:50:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloLogWorldTest {
+
+	static HelloLogWorld clz;
+
+	@BeforeClass
+	public static void setUpOnce() {
+		clz = new HelloLogWorld();
+	}
+
+	@Test
+	public void testPrintLogLevel() {
+		clz.printLogLevel();
+	}
+
+	@Test
+	public void testusingSlf4jPlaceholder() {
+		clz.usingSlf4jPlaceholder(1);
+		clz.usingSlf4jPlaceholder(4);
+		clz.usingSlf4jPlaceholder(5);
+		clz.usingSlf4jPlaceholder(8);
+		clz.usingSlf4jPlaceholder(10);
+		clz.usingSlf4jPlaceholder(200);
+	}
+
+}
