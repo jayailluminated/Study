ca9910f068c689c095ba8d84be7db467e2c053da
JungJoo Seo <moretajoo@gmail.com>
2010-10-30 14:52:14 +0900

    dependency injection in action source

--- java-in-action/dependency-injection/src/main/java/dependency/injection/model/email/SimpleJapaneseTextEditor.java
+++
@@ -1,9 +0,0 @@
-package dependency.injection.model.email;
-
-/**
- * User: jungjooseo
- * Date: Jan 8, 2010
- * Time: 2:03:18 AM
- */
-public class SimpleJapaneseTextEditor extends TextEditor{
-}
--- java-in-action/dependency-injection/src/main/java/dependency/injection/model/email/TextEditor.java
+++
@@ -1,9 +0,0 @@
-package dependency.injection.model.email;
-
-/**
- * User: jungjooseo
- * Date: Jan 8, 2010
- * Time: 2:31:25 AM
- */
-public class TextEditor {
-}
