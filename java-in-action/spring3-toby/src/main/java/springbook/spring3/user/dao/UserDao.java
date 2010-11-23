a3eb3b2fd722464ba02f36e1fc7ab553cd68c9c9
JungJoo Seo <moretajoo@gmail.com>
2010-11-23 16:04:33 +0900

    chapter3 complete

--- java-in-action/spring3-toby/src/main/java/springbook/spring3/user/dao/UserDao.java
+++ java-in-action/spring3-toby/src/main/java/springbook/spring3/user/dao/UserDao.java
@@ -23,12 +23,10 @@ import springbook.user.domain.User;
  */
 public class UserDao {
 
-	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplate;
 
 	public void setDataSource(DataSource dataSource) {
 		this.jdbcTemplate = new JdbcTemplate(dataSource);
-		this.dataSource = dataSource;
 	}
 
 	public int getCount() {
@@ -50,89 +48,32 @@ public class UserDao {
 		});
 	}
 
-	//	public int getCount() throws SQLException {
-	//		Connection c = dataSource.getConnection();
-	//
-	//		PreparedStatement ps = c.prepareStatement("select count(*) from users");
-	//
-	//		ResultSet rs = ps.executeQuery();
-	//		rs.next();
-	//		int count = rs.getInt(1);
-	//
-	//		rs.close();
-	//		ps.close();
-	//		c.close();
-	//		return count;
-	//	}
-
 	public void add(final User user) throws SQLException {
-//		this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-//			@Override
-//			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-//				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-//				ps.setString(1, user.getId());
-//				ps.setString(2, user.getName());
-//				ps.setString(3, user.getPassword());
-//				return ps;
-//			}
-//		});
 		this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
 	}
 
-	//	public User get(String id) throws SQLException {
-	//		Connection c = dataSource.getConnection();
-	//		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-	//		ps.setString(1, id);
-	//
-	//		ResultSet rs = ps.executeQuery();
-	//
-	//		User user = null;
-	//		if (rs.next()) {
-	//			user = new User();
-	//			user.setId(rs.getString("id"));
-	//			user.setName(rs.getString("name"));
-	//			user.setPassword(rs.getString("password"));
-	//
-	//		}
-	//
-	//		if (user == null)
-	//			throw new EmptyResultDataAccessException(1);
-	//
-	//		return user;
-	//	}
 
 	public User get(String id) {
 		return this.jdbcTemplate.queryForObject("select * from users where id = ?",
-				new Object[] { id },
-				new RowMapper<User>() {
-					@Override
-					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-		});
+				new Object[] { id }, this.userMapper);
 	}
 
 	public List<User> getAll() {
-		return this.jdbcTemplate.query("select * from users order by id",
-				new RowMapper<User>() {
-					@Override
-					public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-						User user = new User();
-						user.setId(rs.getString("id"));
-						user.setName(rs.getString("name"));
-						user.setPassword(rs.getString("password"));
-						return user;
-					}
-
-				});
+		return this.jdbcTemplate.query("select * from users order by id", this.userMapper);
 	}
 
+	private final RowMapper<User> userMapper = new RowMapper<User>() {
+		@Override
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
+			user.setId(rs.getString("id"));
+			user.setName(rs.getString("name"));
+			user.setPassword(rs.getString("password"));
+			return user;
+		}
+	};
+
 	public void deleteAll() throws SQLException {
-		// this.jdbcContext.executeSql("delete from users");
 		this.jdbcTemplate.update("delete from users");
 	}
 
