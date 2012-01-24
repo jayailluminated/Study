db3992fa00ba45b382ccaf59d432eb03ff22cb79
JungJoo Seo <moretajoo@gmail.com>
2012-01-25 01:42:41 +0900

    start ruby study

---
+++ rails-in-action/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsInAction::Application.config.secret_token = '956d65463a15470ca06c53c5327427e19738edcc1c9c2d85a0dbd1c639d378ca2ba7008081e75dfa6eec083d19f6d79b2034dfb7b682f477d3db1783cb83064a'
